bb93025f7f00954fbdba418a0f8cea06eb2d2141
MohamadHarastani <mohammad.harastani@gmail.com>
2021-02-22 22:08:31 +0100

    Added an option for setting figure limits

--- continuousflex/viewers/viewer_pdb_dimred.py
+++ continuousflex/viewers/viewer_pdb_dimred.py
@@ -25,7 +25,7 @@
 from os.path import basename
 import numpy as np
 from pwem.emlib import MetaData, MDL_ORDER
-from pyworkflow.protocol.params import StringParam, LabelParam
+from pyworkflow.protocol.params import StringParam, LabelParam, EnumParam, FloatParam
 from pyworkflow.viewer import (ProtocolViewer, DESKTOP_TKINTER, WEB_DJANGO)
 from pyworkflow.utils import replaceBaseExt, replaceExt
 
@@ -37,6 +37,14 @@ import pwem.emlib.metadata as md
 from pyworkflow.utils.process import runJob
 from pwem.viewers import ObjectView
 import matplotlib.pyplot as plt
+
+X_LIMITS_NONE = 0
+X_LIMITS = 1
+Y_LIMITS_NONE = 0
+Y_LIMITS = 1
+Z_LIMITS_NONE = 0
+Z_LIMITS = 1
+
 
 class FlexProtPdbDimredViewer(ProtocolViewer):
     """ Visualization of dimensionality reduction on PDBs
@@ -63,6 +71,41 @@ class FlexProtPdbDimredViewer(ProtocolViewer):
                            'Type 1 2 to see the 2D plot of amplitudes for PCA axes 1 2.\n'
                            'Type 1 2 3 to see the 3D plot of amplitudes for PCA axes 1 2 3; etc.'
                            )
+        form.addParam('xlimits_mode', EnumParam,
+                      choices=['Automatic (Recommended)', 'Set manually x-axis limits'],
+                      default=X_LIMITS_NONE,
+                      label='x-axis limits', display=EnumParam.DISPLAY_COMBO,
+                      help='This allows you to use a specific range of x-axis limits')
+        form.addParam('xlim_low', FloatParam, default=None,
+                      condition='xlimits_mode==%d' % X_LIMITS,
+                      label='Lower x-axis limit')
+        form.addParam('xlim_high', FloatParam, default=None,
+                      condition='xlimits_mode==%d' % X_LIMITS,
+                      label='Upper x-axis limit')
+        form.addParam('ylimits_mode', EnumParam,
+                      choices=['Automatic (Recommended)', 'Set manually y-axis limits'],
+                      default=Y_LIMITS_NONE,
+                      label='y-axis limits', display=EnumParam.DISPLAY_COMBO,
+                      help='This allows you to use a specific range of y-axis limits')
+        form.addParam('ylim_low', FloatParam, default=None,
+                      condition='ylimits_mode==%d' % Y_LIMITS,
+                      label='Lower y-axis limit')
+        form.addParam('ylim_high', FloatParam, default=None,
+                      condition='ylimits_mode==%d' % Y_LIMITS,
+                      label='Upper y-axis limit')
+        form.addParam('zlimits_mode', EnumParam,
+                      choices=['Automatic (Recommended)', 'Set manually z-axis limits'],
+                      default=Z_LIMITS_NONE,
+                      label='z-axis limits', display=EnumParam.DISPLAY_COMBO,
+                      help='This allows you to use a specific range of z-axis limits')
+        form.addParam('zlim_low', FloatParam, default=None,
+                      condition='zlimits_mode==%d' % Z_LIMITS,
+                      label='Lower z-axis limit')
+        form.addParam('zlim_high', FloatParam, default=None,
+                      condition='zlimits_mode==%d' % Z_LIMITS,
+                      label='Upper z-axis limit')
+
+
 
     def _getVisualizeDict(self):
         return {'displayRawDeformation': self._viewRawDeformation}
@@ -75,17 +118,36 @@ class FlexProtPdbDimredViewer(ProtocolViewer):
         components = list(map(int, components.split()))
         # print(components)
         dim = len(components)
-        views = []
-        print(self.protocol.getOutputMatrixFile())
+        if self.xlimits_mode.get() == X_LIMITS:
+            x_low = self.xlim_low.get()
+            x_high = self.xlim_high.get()
+        if self.ylimits_mode.get() == Y_LIMITS:
+            y_low = self.ylim_low.get()
+            y_high = self.ylim_high.get()
+        if self.zlimits_mode.get() == Z_LIMITS:
+            z_low = self.zlim_low.get()
+            z_high = self.zlim_high.get()
+
+        # print(self.protocol.getOutputMatrixFile())
         X = np.loadtxt(fname=self.protocol.getOutputMatrixFile())
         if dim == 1:
             plt.hist(X[:,components[0]-1])
         if dim == 2:
             plt.scatter(X[:,components[0]-1],X[:,components[1]-1])
+            if self.xlimits_mode.get() == X_LIMITS:
+                plt.xlim([x_low,x_high])
+            if self.ylimits_mode.get() == Y_LIMITS:
+                plt.ylim([y_low,y_high])
         if dim == 3:
             fig = plt.figure()
             ax = fig.gca(projection='3d')
             ax.scatter(X[:,components[0]-1],X[:,components[1]-1],X[:,components[2]-1])
+            if self.xlimits_mode.get() == X_LIMITS:
+                ax.set_xlim([x_low,x_high])
+            if self.ylimits_mode.get() == Y_LIMITS:
+                ax.set_ylim([y_low,y_high])
+            if self.zlimits_mode.get() == Z_LIMITS:
+                ax.set_zlim([z_low,z_high])
         plt.show()
 
 
