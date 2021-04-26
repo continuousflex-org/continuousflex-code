c04e77ede6b39adbaa12086d382cf48f64ce2b07
MohamadHarastani <45387413+MohamadHarastani@users.noreply.github.com>
2021-04-26 13:06:05 +0200

    Update __init__.py

--- continuousflex/__init__.py
+++ continuousflex/__init__.py
@@ -32,7 +32,7 @@ getXmippPath = pwem.Domain.importFromPlugin("xmipp3.base", 'getXmippPath')
 from pyworkflow.tests import DataSet
 
 _logo = "logo.png"
-__version__ = "3.0.24"
+__version__ = "3.0.25"
 
 class Plugin(pwem.Plugin):
     _homeVar = CONTINUOUSFLEX_HOME
