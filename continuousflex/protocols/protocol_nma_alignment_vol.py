20bb8443b2f93c484596886d33a536e8140d3c2b
James Krieger <jamesmkrieger@gmail.com>
2022-03-15 15:24:44 +0000

    more typos

--- continuousflex/protocols/protocol_nma_alignment_vol.py
+++ continuousflex/protocols/protocol_nma_alignment_vol.py
@@ -123,14 +123,14 @@ class FlexProtAlignmentNMAVol(ProtAnalysis3D):
         form.addParam('frm_freq', params.FloatParam, default=0.25,
                       expertLevel=params.LEVEL_ADVANCED,
                       label='Maximum cross correlation frequency',
-                      help='The normalized frequency should be between 0 and 0.5 '
-                           'The more it is, the bigger the search frequency is, the more time it demands, '
-                           'keeping it as default is recommended.')
+                      help='The normalized frequency should be between 0 and 0.5 \n'
+                           'The larger it is, the bigger the search frequency is, the more time it demands. '
+                           'Keeping it as default is recommended.')
         form.addParam('frm_maxshift', params.IntParam, default=10,
                       expertlevel=params.LEVEL_ADVANCED,
                       label='Maximum shift for rigid body alignment (in pixels)',
                       help='The maximum shift is a number between 1 and half the size of your volume. '
-                           'It represents the maximum distance searched in x,y and z directions. Keep as default'
+                           'It represents the maximum distance searched in x, y and z directions. Keep as default'
                            ' if your target is near the center in your subtomograms')
         form.addParallelSection(threads=0, mpi=5)
 
