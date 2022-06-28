9a8f96e82a982ad5d451e77475c43b645124b301
ubuntu-laptop <rvuillemot@gmail.com>
2022-06-28 10:35:54 +1000

    aliggnement save as xmd

--- continuousflex/protocols/protocol_pdb_dimred.py
+++ continuousflex/protocols/protocol_pdb_dimred.py
@@ -41,6 +41,7 @@ from joblib import dump
 
 from .utilities.genesis_utilities import dcd2numpyArr
 from .utilities.pdb_handler import ContinuousFlexPDBHandler
+import pwem.emlib.metadata as md
 
 PDB_SOURCE_SUBTOMO = 0
 PDB_SOURCE_PATTERN = 1
@@ -75,37 +76,40 @@ class FlexProtDimredPdb(ProtAnalysis3D):
                       help='Use a scipion object SetOfPDBs / SetOfAtomStructs')
         form.addParam('dcds_file', params.PathParam,
                       condition='pdbSource == 3',
-                      label="List of trajectory DCD files",
+                      label="DCD trajectory file (s)",
                       help='Use the file pattern as file location with /*.dcd')
         form.addParam('dcd_start', params.IntParam, default=0,
                       condition='pdbSource == 3',
                       label="Beginning of the trajectory",
-                      help='Index of the desired begining of the trajectory')
+                      help='Index of the desired begining of the trajectory', expertLevel=params.LEVEL_ADVANCED)
         form.addParam('dcd_end', params.IntParam, default=-1,
                       condition='pdbSource == 3',
                       label="Ending of the trajectory",
-                      help='Index of the desired end of the trajectory')
+                      help='Index of the desired end of the trajectory', expertLevel=params.LEVEL_ADVANCED)
         form.addParam('dcd_step', params.IntParam, default=1,
                       condition='pdbSource == 3',
                       label="Step of the trajectory",
-                      help='Step to skip points in the trajectory')
+                      help='Step to skip points in the trajectory', expertLevel=params.LEVEL_ADVANCED)
         form.addParam('dcd_ref_pdb', params.PointerParam, pointerClass='AtomStruct',
                       condition='pdbSource == 3',
                       label="trajectory Reference PDB",
-                      help='Reference PDB of the trajectory')
+                      help='Reference PDB of the trajectory', expertLevel=params.LEVEL_ADVANCED)
         form.addParam('method', params.EnumParam, label="Reduction method", default=REDUCE_METHOD_PCA,
                       choices=['PCA', 'UMAP'],help="")
 
-        form.addParam('reducedDim', IntParam, default=2,
+        form.addParam('reducedDim', IntParam, default=10,
                       label='Number of Principal Components')
         form.addParam('alignPDBs', params.BooleanParam, default=False,
                       label="Align PDBs ?",
                       help='Perform rigid body alignement on the set of PDBs to a reference PDB')
-        form.addParam('alignRefPDB', params.PointerParam, pointerClass='AtomStruct',
+
+        group = form.addGroup('Alignement parameters', condition="alignPDBs" )
+
+        group.addParam('alignRefPDB', params.PointerParam, pointerClass='AtomStruct',
                       condition='alignPDBs',
                       label="Alignement Reference PDB",
                       help='Reference PDB to align the PDBs with')
-        form.addParam('matchingType', params.EnumParam, label="Match structures ?", default=0,
+        group.addParam('matchingType', params.EnumParam, label="Match structures ?", default=0,
                       choices=['All structures are matching', 'Match chain name/residue num/atom name',
                                'Match segment name/residue num/atom name'],
                       help="Method to find atomic coordinates correspondence between the trajectory "
@@ -116,6 +120,8 @@ class FlexProtDimredPdb(ProtAnalysis3D):
         # --------------------------- INSERT steps functions --------------------------------------------
     def _insertAllSteps(self):
         self._insertFunctionStep('readInputFiles')
+        if self.alignPDBs.get():
+            self._insertFunctionStep('rigidBodyAlignementStep')
         self._insertFunctionStep('performDimred')
 
         if self.method.get() == REDUCE_METHOD_PCA:
@@ -124,18 +130,6 @@ class FlexProtDimredPdb(ProtAnalysis3D):
     # --------------------------- STEPS functions --------------------------------------------
     def readInputFiles(self):
         inputFiles = self.getInputFiles()
-        inputPDB = ContinuousFlexPDBHandler(self.getPDBRef())
-
-        # Align PDBS if needed
-        if self.pdbSource.get() != PDB_SOURCE_TRAJECT:
-            if self.alignPDBs.get():
-                ref = ContinuousFlexPDBHandler(self.alignRefPDB.get().getFileName())
-                if self.matchingType.get() == 1:
-                    idx_matching_atoms = inputPDB.matchPDBatoms(reference_pdb=ref, matchingType=0)
-                elif self.matchingType.get() == 2:
-                    idx_matching_atoms = inputPDB.matchPDBatoms(reference_pdb=ref, matchingType=1)
-                else:
-                    idx_matching_atoms = None
 
         # Get pdbs coordinates
         pdbs_matrix = []
@@ -151,22 +145,58 @@ class FlexProtDimredPdb(ProtAnalysis3D):
                 try :
                     # Read PDBs
                     mol = ContinuousFlexPDBHandler(pdbfn)
-
-                    # Align PDBs
-                    if self.alignPDBs.get():
-                        mol= mol.alignMol(reference_pdb=ref, idx_matching_atoms=idx_matching_atoms)
-
                     pdbs_matrix.append(mol.coords)
 
                 except RuntimeError:
                     print("Warning : Can not read PDB file %s "%pdbfn)
 
         pdbs_arr = np.array(pdbs_matrix)
-
         # save as dcd file
         numpyArr2dcd(pdbs_arr, self._getExtraPath("coords.dcd"))
 
+    def rigidBodyAlignementStep(self):
 
+        # open files
+        inputPDB = ContinuousFlexPDBHandler(self.getPDBRef())
+        refPDB = ContinuousFlexPDBHandler(self.alignRefPDB.get().getFileName())
+        arrDCD = dcd2numpyArr(self._getExtraPath("coords.dcd"))
+        nframe, natom,_ =arrDCD.shape
+        alignXMD = md.MetaData()
+
+        # find matching index between reference and pdbs
+        if self.matchingType.get() == 1:
+            idx_matching_atoms = inputPDB.matchPDBatoms(reference_pdb=refPDB, matchingType=0)
+        elif self.matchingType.get() == 2:
+            idx_matching_atoms = inputPDB.matchPDBatoms(reference_pdb=refPDB, matchingType=1)
+        else:
+            idx_matching_atoms = None
+
+        # loop over all pdbs
+        for i in range(nframe):
+
+            # rotate
+            if self.matchingType.get() != 0 :
+                ref_coord = refPDB.coords[idx_matching_atoms[:, 1]]
+                coord = arrDCD[i][idx_matching_atoms[:, 0]]
+            else:
+                ref_coord = refPDB.coords
+                coord = arrDCD[i]
+            rot_mat, tran = ContinuousFlexPDBHandler.alignCoords(ref_coord, coord)
+            arrDCD[i] = np.dot(arrDCD[i], rot_mat) + tran
+
+            # add to MD
+            shftx, shfty, shftz = tran
+            rot, tilt, psi, = matrix2eulerAngles(rot_mat)
+            index = alignXMD.addObject()
+            alignXMD.setValue(md.MDL_ANGLE_ROT, rot, index)
+            alignXMD.setValue(md.MDL_ANGLE_TILT, tilt, index)
+            alignXMD.setValue(md.MDL_ANGLE_PSI, psi, index)
+            alignXMD.setValue(md.MDL_SHIFT_X, shftx, index)
+            alignXMD.setValue(md.MDL_SHIFT_Y, shfty, index)
+            alignXMD.setValue(md.MDL_SHIFT_Z, shftz, index)
+
+        numpyArr2dcd(arrDCD, self._getExtraPath("coords.dcd"))
+        alignXMD.write(self._getExtraPath("alignement.xmd"))
 
     def performDimred(self):
 
@@ -271,3 +301,32 @@ class FlexProtDimredPdb(ProtAnalysis3D):
             with open("%s/vec.%i"%(prefix,i+1), "w") as f:
                     for j in range(matrix.shape[1]):
                         f.write(" %e   %e   %e\n" % (matrix[i,j, 0], matrix[i,j, 1], matrix[i,j, 1]))
+
+
+def matrix2eulerAngles(A):
+    abs_sb = np.sqrt(A[0, 2] * A[0, 2] + A[1, 2] * A[1, 2])
+    if (abs_sb > 16 * np.exp(-5)):
+        gamma = np.arctan2(A[1, 2], -A[0, 2])
+        alpha = np.arctan2(A[2, 1], A[2, 0])
+        if (abs(np.sin(gamma)) < np.exp(-5)):
+            sign_sb = np.sign(-A[0, 2] / np.cos(gamma))
+        else:
+            if np.sin(gamma) > 0:
+                sign_sb = np.sign(A[1, 2])
+            else:
+                sign_sb = -np.sign(A[1, 2])
+        beta = np.arctan2(sign_sb * abs_sb, A[2, 2])
+    else:
+        if (np.sign(A[2, 2]) > 0):
+            alpha = 0
+            beta = 0
+            gamma = np.arctan2(-A[1, 0], A[0, 0])
+        else:
+            alpha = 0
+            beta = np.pi
+            gamma = np.arctan2(A[1, 0], -A[0, 0])
+    gamma = np.rad2deg(gamma)
+    beta = np.rad2deg(beta)
+    alpha = np.rad2deg(alpha)
+    return alpha, beta, gamma
+
